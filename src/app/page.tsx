825255c01c42c25b821364b5a2924bfd06f3a857
tgt <mashoor539@icloud.com>
2024-03-23 20:06:23 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 					</div>
 					<ul className="list-disc translate-x-5">
 						<li>
-							AnyWeb - A web proxy with a sleek modern UI, and with features
+							Ephemeral - A web proxy with a sleek modern UI, and with features
 							such as userscripts and userstyles.
 						</li>
 						<li>
@@ -40,14 +40,13 @@ export default function Home() {
 							contributed to
 						</li>
 						<li>Stardust - An open source Kasm alternative in the works.</li>
-						<li>AnyCode - A service for serverless functions, and server like deployments, like Vercel or Render</li>
 					</ul>
 					<div className="text-xl text-ctp-text font-bold">
 						Devteams I&apos;m in
 					</div>
 					<ul className="list-disc translate-x-5">
 						<li>Interstellar - member</li>
-						<li>AnyVM - Member</li>
+						<li>z1g Project - Member</li>
 						<li>Spaceness - Owner</li>
 					</ul>
 					<div className="text-xl text-ctp-text font-bold">
